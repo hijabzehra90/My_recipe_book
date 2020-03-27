a332957a4eeae639c1afc35809d305da626bc3f4
Hijab <hijabzehra90@gmail.com>
2020-03-27 12:35:51 +0500

    recyclerView implemented

--- app/src/main/java/com/example/myrecipebook/MainActivity.java
+++ app/src/main/java/com/example/myrecipebook/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -16,13 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            RecyclerView recyclerView = findViewById(R.id.recycler_view);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setHasFixedSize(true);
+
+            final RecipeAdaptor adapter = new RecipeAdaptor();
+            recyclerView.setAdapter(adapter);
+
+
         //have to add gradle dependencies for ViewModelProviders
         recipeViewModel = new ViewModelProvider(this).get(RecipeViewModel.class);
         recipeViewModel.getAllRecipes().observe(this, new Observer<List<Recipe>>() {
             @Override
             public void onChanged(List<Recipe> recipes) {
                 // update recyclerview here later
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+
+                //updated
+
+                adapter.setRecipes(recipes);
             }
         });
     }
